6bf46ff1a0d1f2b18aba0f066479f14dc0a2cf61
Eric Toler <etoler1841@gmail.com>
2018-06-29 17:52:49 -0500

    Revert "Updated DB connection"
    This reverts commit 2b5f7068152a1dfe69763d8e340d0c38bd90ef45.

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,7 @@
   $pass = 'b3n3931206pr!ce';
   $db = 'pbgames_zencart2';
 
-  $conn = new mysqli($host, $user, $pass, $db);
+  $conn = new mysqli('148.72.29.202', 'pbgames_test', 'b3n3931206pr!ce', 'pbgames_testing_db');
 
   if($conn->errno){
     echo $conn->error;
